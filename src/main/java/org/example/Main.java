f4e9442b0fc95b3e942575cca4b9f80a244dcd9e
Michael <ananevr4@gmail.com>
2024-04-10 18:28:39 +0300

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        String ім_я = "Вася";
+        int вік = 168;
+        float вага = 60.5f;
+
+        System.out.println("Ім'я=" + ім_я + ", Вік=" + вік + ", Вага=" + вага);
+        int a = 1;
+        int b = 2;
+        a = a + b;
+        b = a - b;
+        a = a - b;
+        System.out.println("a: " + a);
+        System.out.println("b: " + b);
+        int x = 5;
+        int y = 3;
+        x = x + y;
+        y = x - y;
+        x = x - y;
+        System.out.println("x: " + x);
+        System.out.println("y: " + y);
+    }
+}
